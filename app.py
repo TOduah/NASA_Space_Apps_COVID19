57e6773284f72317ade6d613ac28869076a3bf06
Cat Nguyen <ctn57d@gmail.com>
2020-05-30 17:15:22 -0400

    Added numpy and pandas to dependencies

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
+import numpy
+import pandas
+
 app = Flask(__name__)
+
 @app.route('/', methods=['GET'])
+
 def index_page_landing():
     return render_template('index.html')
+
 if __name__ == "__main__":
     app.run()
